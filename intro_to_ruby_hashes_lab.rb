c1c74d5f79a301a411f8783e3881a8d2aafbec81
Adam Brown <adam.brown.betty@gmail.com>
2019-08-01 21:32:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,9 @@ end
 def monopoly_with_third_tier
       monopoly = {
    :railroads => {:rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200},
-   :names => {:reading_railroad => {}, :key2 => 5, :key3 => 6, :key4 => 7}
+   :names => {:reading_railroad => {}, :pennsylvania_railroad => {}, :key3 => {}, :key4 => {}
+     
+   }
       
         
     }
